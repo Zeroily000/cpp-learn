af83656ef224edd639e5d3c320c4930052cc78d1
Zhongjian Zhu <zhuzhongjian321@gmail.com>
2019-11-26 23:08:38 -0500

    Added comments to 2d_array.

--- 2d_array/2d_array.cpp
+++ 2d_array/2d_array.cpp
@@ -1,33 +1,38 @@
+/**
+ * @note Mutidimensional arrays are not multi-pointer. This line of code is incorrect:
+ *       int **ptr = (int**)matrix;
+ *       When arrays are passed to functions, they decay to pointers to their first element.
+ *       For a 2d array, this would be int (*)[cols].
+ */
 #include <iostream>
 
-template <typename T, size_t length>
+template <typename T, std::size_t length>
 void printVector(T const * const vec) {
-    for (size_t i{0}; i < length; ++i)
+    for (std::size_t i{0}; i < length; ++i)
         std::cout << vec[i] << " ";
     std::cout << std::endl;
 }
 
-template <typename T, size_t rows, size_t cols>
-void print2DArray(T const arr[rows][cols]) {
-    for (size_t i{0}; i < rows; ++i)
+template <typename T, std::size_t rows, std::size_t cols>
+void print2DArray1(T const arr[rows][cols]) {
+    for (std::size_t i{0}; i < rows; ++i)
         printVector<T, cols>(arr[i]);
     std::cout << std::endl;
 }
 
-template <typename T, size_t rows, size_t cols>
-void print2DArray(T (* const arr)[cols]) {
-    for (size_t i{0}; i < rows; ++i)
+template <typename T, std::size_t rows, std::size_t cols>
+void print2DArray2(T const (* const arr)[cols]) {
+    for (std::size_t i{0}; i < rows; ++i)
         printVector<T, cols>(arr[i]);
     std::cout << std::endl;
 }
 
-int main() {
-
-    size_t constexpr rows{2}, cols{3};
+int main(int argc, char ** argv) {
+    std::size_t constexpr rows{2}, cols{3};
     int constexpr arr[rows][cols] = {{1,2,3}, {4,5,6}};
-    print2DArray<int, rows, cols>(arr);
-    print2DArray<int, rows, cols>((int (* const) [cols])arr);
 
+    print2DArray1<int, rows, cols>(arr);
+    print2DArray2<int, rows, cols>(arr);
 
     return 0;
 }
