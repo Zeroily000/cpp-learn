409d8abdc7f01bbf5be6b0ab5e842619fb9799b0
Zhongjian Zhu <zhuzhongjian321@gmail.com>
2019-12-08 21:27:28 -0500

    Made alias a unit test

--- alias/alias.cpp
+++ alias/alias.cpp
@@ -1,34 +1,49 @@
 #include <iostream>
-#include <typeinfo>
+#include <type_traits>
 #include "boost/core/demangle.hpp"
+#include "gtest/gtest.h"
 
 typedef unsigned long ulong;
 
-typedef int int_t, *intp_t, (&fp)(int, ulong), arr_t[10];
+typedef int int_t, *intp_t, (&fr)(int, ulong), (*fp)(int, ulong), arr_t[10];
 
 typedef struct {int a; int b;} S, *pS;
 
-long unsigned typedef int long ullong;
+long unsigned typedef long ullong;
 
 template <typename T>
 struct add_const {
     typedef T const type;
 };
 
-int main(int argc, char** argv)
+TEST(ALIAS, type_define) {
+    ASSERT_TRUE(static_cast<bool>(std::is_same<ulong, unsigned long>::value))
+        << "ulong: " << boost::core::demangle(typeid(ulong).name());
+
+    ASSERT_TRUE(static_cast<bool>(std::is_same<int_t, int>::value))
+        << "int_t: " << boost::core::demangle(typeid(int_t).name());
+
+    ASSERT_TRUE(static_cast<bool>(std::is_same<intp_t, int*>::value))
+        << "intp_t: " << boost::core::demangle(typeid(intp_t).name());
+
+    ASSERT_TRUE(static_cast<bool>(std::is_same<fr, int(&)(int, unsigned long)>::value))
+        << "fp: " << boost::core::demangle(typeid(fr).name());
+
+    ASSERT_TRUE(static_cast<bool>(std::is_same<fp, int(*)(int, unsigned long)>::value))
+        << "fp: " << boost::core::demangle(typeid(fp).name());
+
+    ASSERT_TRUE(static_cast<bool>(std::is_same<arr_t , int[10]>::value))
+        << "fp: " << boost::core::demangle(typeid(arr_t).name());
+
+    ASSERT_TRUE(static_cast<bool>(std::is_same<pS, S*>::value))
+        << "fp: " << boost::core::demangle(typeid(pS).name());
+
+    ASSERT_TRUE(static_cast<bool>(std::is_same<add_const<int>::type, int const>::value))
+        << "fp: " << boost::core::demangle(typeid(add_const<int>::type).name());
+}
+
+int main(int argc, char **argv)
 {
-    std::cout << "ulong: " << boost::core::demangle(typeid(ulong).name()) << std::endl;
-
-    std::cout << "int_t: " << boost::core::demangle(typeid(int_t).name()) << std::endl;
-    std::cout << "intp_t: " << boost::core::demangle(typeid(intp_t).name()) << std::endl;
-    std::cout << "fp: " << boost::core::demangle(typeid(fp).name()) << std::endl;
-    std::cout << "arr_t: " << boost::core::demangle(typeid(arr_t).name()) << std::endl;
-
-    std::cout << "S: " << boost::core::demangle(typeid(S).name()) << std::endl;
-    std::cout << "pS: " << boost::core::demangle(typeid(pS).name()) << std::endl;
-
-    std::cout << "ullong: " << boost::core::demangle(typeid(ullong).name()) << std::endl;
-
-    std::cout << "add_const<int>::type: " << boost::core::demangle(typeid(add_const<int>::type).name()) << std::endl;
-    return 0;
+    testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
 }
