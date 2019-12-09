805938d1c7856c8a329c042ebea11dd60a2b54e8
Zhongjian Zhu <zhuzhongjian321@gmail.com>
2019-12-08 20:52:59 -0500

    Made binary search a unit test

--- binary_search/binary_search.cpp
+++ binary_search/binary_search.cpp
@@ -1,11 +1,11 @@
-#include <iostream>
 #include <vector>
+#include "gtest/gtest.h"
 
 template <typename T>
 int findLeft(std::vector<T> const & nums, T const target) {
-    size_t lo{0}, hi{nums.size()-1};
+    std::size_t lo{0}, hi{nums.size()-1};
     while (lo < hi) {
-        size_t mi{lo + (hi - lo)/2};
+        std::size_t mi{lo + (hi - lo)/2};
         if (nums[mi] < target) lo = mi + 1;
         else hi = mi;
     }
@@ -14,31 +14,28 @@ int findLeft(std::vector<T> const & nums, T const target) {
 
 template <typename T>
 int findRight(std::vector<T> const & nums, T const target) {
-    size_t lo{0}, hi{nums.size()-1};
+    std::size_t lo{0}, hi{nums.size()-1};
     while (lo < hi) {
-        size_t mi{lo + (hi - lo + 1)/2};
+        std::size_t mi{lo + (hi - lo + 1)/2};
         if (nums[mi] > target) hi = mi - 1;
         else lo = mi;
     }
     return lo;
 }
 
+static std::vector<int> const nums{0,1,2,4,4,4,6};
+
+TEST(BINARY_SEARCH, find_existing_element) {
+    ASSERT_EQ(findLeft(nums, 4), 3);
+    ASSERT_EQ(findRight(nums, 4), 5);
+}
+
+TEST(BINARY_SEARCH, find_nonexisting_element) {
+    ASSERT_EQ(findLeft(nums, 5), 6);
+    ASSERT_EQ(findRight(nums, 3), 2);
+}
+
 int main(int argc, char ** argv) {
-    std::vector<int> const nums{0,1,2,3,3,3,6};
-    std::cout << "nums: ";
-    for (int const & num : nums) std::cout << num << ' ';
-    std::cout << std::endl;
-
-    int idx;
-    idx = findLeft(nums, 3);
-    std::cout << "findLeft(nums, 3) = " << idx << ", nums[idx] = " << nums[idx] << std::endl;
-    idx = findRight(nums, 3);
-    std::cout << "findRight(nums, 3) = " << idx << ", nums[idx] = " << nums[idx] << std::endl;
-
-    idx = findLeft(nums, 4);
-    std::cout << "findLeft(nums, 4) = " << idx << ", nums[idx] = " << nums[idx] << std::endl;
-    idx = findRight(nums, 4);
-    std::cout << "findRight(nums, 4) = " << idx << ", nums[idx] = " << nums[idx] << std::endl;
-    return 0;
-
+    testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
 }
