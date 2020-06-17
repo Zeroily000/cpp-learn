758acd90e7b5ec266f365a801c749a29a6761d59
Zhongjian Zhu <zhuzhongjian321@gmail.com>
2020-06-16 22:55:29 -0700

    Removed parameter pack

--- python/wrapper.cpp
+++ python/wrapper.cpp
@@ -1,22 +1,22 @@
 #include <pybind11/pybind11.h>
 #include <pybind11/stl.h>
 
-#include "cpp-learn.hpp"
+//#include "cpp-learn.hpp"
 
 namespace py = pybind11;
 
 PYBIND11_MODULE(cppLearnPyLib, m) {
     m.doc() = "python binding example";
 
-    py::class_<CppLearn>(m, "CppLearn")
-        .def(py::init<>())
-        .def_static("findLeft", &CppLearn::findLeft<int>,
-                    "Binary search a sorted array, return the first index of the target if exists, otherwise, return the index of the next largest element",
-                    py::arg("nums"), py::arg("target"))
-        .def_static("findRight", &CppLearn::findRight<int>,
-                    "Binary search a sorted array, return the last index of the target it exists, otherwise, return the index of the next smallest element",
-                    py::arg("nums"), py::arg("target"))
-        ;
+//    py::class_<CppLearn>(m, "CppLearn")
+//        .def(py::init<>())
+//        .def_static("findLeft", &CppLearn::findLeft<int>,
+//                    "Binary search a sorted array, return the first index of the target if exists, otherwise, return the index of the next largest element",
+//                    py::arg("nums"), py::arg("target"))
+//        .def_static("findRight", &CppLearn::findRight<int>,
+//                    "Binary search a sorted array, return the last index of the target it exists, otherwise, return the index of the next smallest element",
+//                    py::arg("nums"), py::arg("target"))
+//        ;
 
     m.attr("the_answer") = 56;
     py::object world = py::cast("World");
--- test/python_binding/test.py
+++ test/python_binding/test.py
@@ -5,15 +5,15 @@ import unittest
 import cppLearnPyLib
 
 
-class TestCppLearn(unittest.TestCase):
-    def testBinarySearch(self):
-        nums = [0,1,2,4,4,4,6]
-        self.assertEqual(cppLearnPyLib.CppLearn().findLeft(nums, 4), 3)
-        self.assertEqual(cppLearnPyLib.CppLearn().findRight(nums, 4), 5)
-        self.assertEqual(cppLearnPyLib.CppLearn.findLeft(nums, 5), 6)
-        self.assertEqual(cppLearnPyLib.CppLearn.findRight(nums, 3), 2)
+# class TestCppLearn(unittest.TestCase):
+#     def testBinarySearch(self):
+#         nums = [0,1,2,4,4,4,6]
+#         self.assertEqual(cppLearnPyLib.CppLearn().findLeft(nums, 4), 3)
+#         self.assertEqual(cppLearnPyLib.CppLearn().findRight(nums, 4), 5)
+#         self.assertEqual(cppLearnPyLib.CppLearn.findLeft(nums, 5), 6)
+#         self.assertEqual(cppLearnPyLib.CppLearn.findRight(nums, 3), 2)
 
 if __name__ == '__main__':
     print(cppLearnPyLib.the_answer)
     print(cppLearnPyLib.what)
-    unittest.main()
+    # unittest.main()
